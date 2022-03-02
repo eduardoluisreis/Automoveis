2bbc9efb435dbd27f09d1b9e00e49ca85bdc859a
Eduardo_Reis <eduardoluisreis@gmail.com>
2022-03-02 00:18:07 -0300

    Alguma Correçoes Textuais

--- src/main/java/Veiculos/Main.java
+++ src/main/java/Veiculos/Main.java
@@ -3,7 +3,7 @@ package Veiculos;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Entre comos Dados do Veiculo");
+        System.out.println("Entre com os Dados do Veiculo");
         Carros carro = new Carros();
 
     }
